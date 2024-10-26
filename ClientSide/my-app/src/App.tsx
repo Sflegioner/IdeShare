31feaa14d4c723dce0db8e56f2d68814e19ddfbf
Sflegioner <117868151+Sflegioner@users.noreply.github.com>
2024-10-26 06:20:46 +0200

    NewApiServer

--- ClientSide/my-app/src/App.tsx
+++ ClientSide/my-app/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Register_Form,Login_Form} from './components/register_login_components'
 import './App.css';
 
 function App() {
   return (
     <div>
-
+      <Register_Form></Register_Form>
+      <Login_Form></Login_Form>
     </div>
   );
 }
--- ClientSide/my-app/src/managers/user_client.tsx
+++ ClientSide/my-app/src/managers/user_client.tsx
@@ -1,35 +1,60 @@
-import fetch from 'node-fetch';
-
-interface UserInterface {
-    username: string,
-    useremail: string,
-    userpass: number,
+export interface UserInterface {
+    username: string;
+    useremail: string;
+    userpass: string;
 }
 
 class UserClient {
-    baseAPIurl = "http://localhost:4444/API";
-    /** */
-    async GetUser(useremail: String): Promise<UserInterface> {
-        const response = await fetch(`${this.baseAPIurl}/user?useremail=${useremail}`, {method: 'GET',});
-        const data = await response.json() as any;
-        const user: UserInterface = {
-            username: data.username,
-            useremail: data.usermail,
-            userpass: data.userpass,
-        }
-        console.log('sucssessfully get User' + user)
-        return user;
-    }
-    async PostUser(username: String, useremail: String, userpass: number): Promise<UserInterface> {
-        const response = await fetch(this.baseAPIurl + '/user', { method: 'POST', body: JSON.stringify({ username: username, useremail: useremail, userpass }) });
-        const data = await response.json() as any;
-        const user: UserInterface = {
-            username: data.username,
-            useremail: data.usermail,
-            userpass: data.userpass,
-        }
-        console.log('sucssessfully add User to db' + user)
+    private baseAPIurl = "http://localhost:4444/API";
+    async GetUser(useremail: string): Promise<UserInterface> {
+        const response = await fetch(`${this.baseAPIurl}/user?useremail=${useremail}`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json', },
+            credentials: 'include'
+        });
+        const user: UserInterface = await response.json();
+        console.log('Successfully fetched user:', user);
         return user;
     }
 
+    async PostUser(new_user: UserInterface): Promise<UserInterface>;
+    async PostUser(username: string, useremail: string, userpass: string): Promise<UserInterface>;
+    async PostUser(usernameOrUser: string | UserInterface, useremail?: string, userpass?: string): Promise<UserInterface> {
+        let body: UserInterface;
+        if (typeof usernameOrUser === 'object') {
+            body = usernameOrUser;
+        } else {
+            body = {
+                username: usernameOrUser,
+                useremail: useremail as string,
+                userpass: userpass as string
+            };
+        }
+
+        const response = await fetch(`${this.baseAPIurl}/user`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json',},
+            body: JSON.stringify(body),
+            credentials: 'include'
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error adding user: ${response.statusText}`);
+        }
+        const user: UserInterface = await response.json(); 
+        console.log('Successfully added user to DB:', user);
+        return user;
+
+    }
+
+
+    async VerefyPassword(password:number, email:string):Promise<any>{
+        const reponse = await fetch(`${this.baseAPIurl}/user`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json', },
+            credentials: 'include'
+        });
+    }
 }
+
+export default UserClient;
